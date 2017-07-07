f16fad2740bd2a1bf0bfaa82baae17c236c7df97
Kyle Kwiedacz <kkwiedacz321@gmail.com>
2017-07-06 20:21:20 -0700

    Added in a route for our add review page

--- client/js/Router.js
+++ client/js/Router.js
@@ -1,13 +1,17 @@
 import React from 'react';
-import { BrowserRouter, Route } from 'react-router-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import App from './components/App';
 import Home from './components/Home';
+import AddReview from './components/AddReview';
 
 export default function Router() {
   return (
     <BrowserRouter>
       <App>
-        <Route exact path="/" component={Home}/>
+        <Switch>
+          <Route exact path="/" component={Home}/>
+          <Route exact path="/addReview" component={AddReview}/>
+        </Switch>
       </App>
     </BrowserRouter>
   );
