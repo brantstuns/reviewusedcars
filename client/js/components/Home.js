a9b351d0453ccda05786d14da0353691ca013aa9
Kyle Kwiedacz <kkwiedacz321@gmail.com>
2017-07-06 20:23:21 -0700

    Added footer styling and components and implemented them on the correct parent pages

--- client/js/components/Home.js
+++ client/js/components/Home.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Header from './Header';
 import Nav from './Nav';
 import Section from './Section';
+import Footer from './Footer';
 
 export default function Home() {
   return (
@@ -10,6 +11,7 @@ export default function Home() {
       <Header headerId='header1' />
       <Section />
       <Header headerId='header2' />
+      <Footer />
     </div>
   );
 }
